937d0849febcd91ef9c42ca839a8fa79903f67e3
Ryan Becwar <rbecwar@cs.colostate.edu>
2017-12-04 21:53:15 -0700

    implementing routing algorithm, added nimrata's code

--- src/segmentCollector/router.py
+++ src/segmentCollector/router.py
@@ -12,14 +12,14 @@ def dist(lat, lon, tarlat, tarlon):
 def route(lat, lon, distance, threshold, segments, cutoffratio):
 
     #a random direction, and a second +- 60 degrees
-    dir_radians = random.uniform(0.0, 2.0*pi)
-    dir2_radians = (dir_radians + random.choice([-1.0, 1.0]) * pi / 3.0) % (2.0 * pi)
+    dir_radians = random.uniform(0.0, 2.0*math.pi)
+    dir2_radians = (dir_radians + random.choice([-1.0, 1.0]) * math.pi / 3.0) % (2.0 * math.pi)
 
     length = distance / 3.0
-    x1off = length * cos(dir_radians) * LON_PER_M
-    y1off = length * sin(dir_radians) * LAT_PER_M
-    x2off = length * cos(dir2_radians) * LON_PER_M
-    y2off = length * sin(dir2_radians) * LAT_PER_M
+    x1off = length * math.cos(dir_radians) * LON_PER_M
+    y1off = length * math.sin(dir_radians) * LAT_PER_M
+    x2off = length * math.cos(dir2_radians) * LON_PER_M
+    y2off = length * math.sin(dir2_radians) * LAT_PER_M
 
     lon1 = lon + x1off
     lat1 = lat + y1off
@@ -29,17 +29,24 @@ def route(lat, lon, distance, threshold, segments, cutoffratio):
     #TODO: find solution to make dummy start searchable by path algorithm one option would be hardcode check for id 1
     #solution: add dummy segment with constant id 1, add to dict, remove 1st segment of route when route is returned, which is dummy
     dummy_start = Segment.dummy(lat, lon)
-    path1 = search(dummy_start.id, lat1, lon1, threshold, segments, cutoffratio)
+    for seg in segments:
+        if dist(lat, lon, segments[seg].start_latitude, segments[seg].start_longitude) <= threshold:
+            dummy_start.links.append(seg)
+    segments[1] = dummy_start
+
+    path1 = search(dummy_start.id, lat1, lon1, threshold, segments, cutoffratio, set([]))[1:]#remove first elem
     if not path1:
         return []
-    path2 = search(path1[-1], lat2, lon2, threshold, segments, cutoffratio)
+    path2 = search(path1[-1], lat2, lon2, threshold, segments, cutoffratio, set(path1))[1:]
     if not path2:
         return []
-    path3 = search(path2[-1], lat, lon, threshold, segments, cutoffratio)
+    path3 = search(path2[-1], lat, lon, threshold, segments, cutoffratio, set(path1) | set(path2))[1:]
     if not path3:
         return []
 
-    return path1.extend(path2).extend(path3)
+    path1.extend(path2)
+    path1.extend(path3)
+    return path1
 
 def goal(segid, targetlat, targetlon, threshold, segments):
     seg = segments[segid]
@@ -50,33 +57,33 @@ def goal(segid, targetlat, targetlon, threshold, segments):
     
 #Breadth first search generator which returns all possible paths that end within range of target
 #location which do not exceed cutoff in total distance
-def bfs_paths(startsegid, targetlat, targetlon, threshold, segments, cutoff):
+def bfs_paths(startsegid, targetlat, targetlon, threshold, segments, cutoff, visited):
     queue = [(startsegid, [startsegid], segments[startsegid].distance)]
     while queue:
         (vertex, path, distance) = queue.pop(0)
         vertseg = segments[vertex]
-        for next in set(vertseg.links) - set(path):
+        for next in set(vertseg.links) - set(path) - visited:
             #only add further segments while the distance threshold has not been exceeded
             nextseg = segments[next]
             newdist = distance + segments[next].distance + dist(vertseg.end_latitude, vertseg.end_longitude, nextseg.start_latitude, nextseg.start_longitude)
             if newdist <= cutoff:
                 if goal(next, targetlat, targetlon, threshold, segments):
-                    yield path + [next]
+                    yield (path + [next], newdist)
                 else:
                     queue.append((next, path + [next], newdist))
 
 
 #function to search from a segment to a geographic point on the graph
-def search(startsegid, targetlat, targetlon, threshold, segments, cutoffratio):
+def search(startsegid, targetlat, targetlon, threshold, segments, cutoffratio, visited):
     cutoff = dist(segments[startsegid].start_latitude, segments[startsegid].start_longitude, targetlat, targetlon) * cutoffratio
     bestpath = []
-    bestscore = 0.0
-    for path in bfs_paths(startsegid, targetlat, targetlon, threshold, segments, cutoff):
-        score = evalPath(path, segments)
+    bestscore = -1.0
+    for path in bfs_paths(startsegid, targetlat, targetlon, threshold, segments, cutoff, visited):
+        score = evalPath(path[0], segments)
         if score > bestscore:
-            bestpath = path
+            bestpath = path[0]
             bestscore = score
-        print(path, score)
+        print(path[0], score, path[1])
         
         #keep track of highest scoring path
     return bestpath
@@ -86,19 +93,26 @@ def evalPath(path, segments):
     #should expand upon this, star count will do for now
     starcount = 0.0
     for seg in path:
-        starcount += segments[seg].star_count
+        #starcount += segments[seg].star_count
+        starcount += segments[seg].athlete_count * segments[seg].distance
 
     return starcount
 
-def run(segmentsfile, distance):
+def run(segmentsfile, distance, threshold, cutoffratio):
     segments = load_segments_dictionary(segmentsfile)
 
-    #route = route(40.5, -105, segments)
-    #print(route)
+    testroute = route(40.55, -105.10, distance, threshold,  segments, cutoffratio)
+    print(testroute)
+    coords = []
+    for seg in testroute:
+        coords.append(((segments[seg].start_latitude, segments[seg].start_longitude), (segments[seg].end_latitude, segments[seg].end_longitude)))
+    print(coords)
 
+
+    return testroute
     #test specifically for tiny dataset
-    path = search(5660941, 40.475605, -105.148393, 10, segments, 50.0)
-    print(path)
+    #path = search(5660941, 40.475605, -105.148393, 10, segments, cutoffratio)
+    #print(path)
 
 
 if __name__ == "__main__":
